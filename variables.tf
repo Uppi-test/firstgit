e69316b4cc4da4789af0cbe1873d68d16f10706b
Uppi-test <varkalaupendar@gmail.com>
2024-12-06 14:59:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,10 +3,25 @@ variable "cidrange" {
 }
 
 variable "cidrpubone" {
-    default = "10.0.0.0/24"
+    default = "10.0.1.0/24"
 }
 
+variable "cidrpubtwo" {
+    default = "10.0.5.0/24"
+}
 
+variable "cidrprione" {
+    default = "10.0.3.0/24"
+}
+
+variable "cidrpritwo" {
+    default = "10.0.4.0/24"
+}
+
+variable "cidroute1" {
+    default = "0.0.0.0/0"
+  
+}
 variable "cidroute" {
     default = "0.0.0.0/0"
   
